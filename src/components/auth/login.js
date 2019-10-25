4b387f20bc8721054a47b6afd22dea08e16dad50
Jonny Smith <jonnyts235@gmail.com>
2019-10-25 10:39:15 -0600

    Finished Portfolio

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default class Login extends Component {
   constructor(props) {
@@ -25,7 +26,7 @@ export default class Login extends Component {
   handleSubmit(event) {
     axios
       .post(
-        "https://api.devcamp.space/sessions",
+        "https://jonnysmith.devcamp.space/sessions",
         {
           client: {
             email: this.state.email,
@@ -59,25 +60,31 @@ export default class Login extends Component {
 
         <div>{this.state.errorText}</div>
 
-        <form onSubmit={this.handleSubmit}>
-          <input
-            type="email"
-            name="email"
-            placeholder="Your Email"
-            value={this.state.email}
-            onChange={this.handleChange}
-          />
-
-          <input
-            type="password"
-            name="password"
-            placeholder="Your password"
-            value={this.state.password}
-            onChange={this.handleChange}
-          />
-          <div>
-            <button type="submit">Login</button>
+        <form onSubmit={this.handleSubmit} className="auth-form-wrapper">
+          <div className="form-group">
+            <FontAwesomeIcon icon="envelope" />
+            <input
+              type="email"
+              name="email"
+              placeholder="Your email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
           </div>
+          <div className="form-group">
+            <FontAwesomeIcon icon="lock" />
+            <input
+              type="password"
+              name="password"
+              placeholder="Your password"
+              value={this.state.password}
+              onChange={this.handleChange}
+            />
+          </div>
+
+          <button className="btn" type="submit">
+            Login
+          </button>
         </form>
       </div>
     );
